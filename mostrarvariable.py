137b0650d1021e4006fa8a05e31553de1ef8e65a
JonathanAdrianBautistaMartinez <61531192+JonathanAdrianBautistaMartinez@users.noreply.github.com>
2020-03-09 20:42:04 -0600

    Create mostrarvariable.py

---
+++ mostrarvariable.py
@@ -0,0 +1,7 @@
+# Realiza la declaracion de una variable, y la muestra.
+
+def  main ():
+  # En python se declara una variable por asignación. 
+  #El datatype depende del valor que se asigna.
+  txt = "Hola mundo, con variable"
+  imprimir ( txt )
